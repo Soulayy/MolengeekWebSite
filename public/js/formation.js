20d224c0975cfeb4579ae48260b052d6d12053a7
Soulayy <s.sultaway@gmail.com>
2024-02-01 10:54:13 +0100

    maj

--- public/js/formation.js
+++ public/js/formation.js
@@ -2,12 +2,18 @@ import data from './../json/molengeek.json' assert { type: "json" };
 
 let textDeFormation = document.querySelectorAll(".botMid")[0].querySelectorAll("ul")[0]
 
-let formationL = Object.keys(data.formations.formationsLongues)
+let formationL = Object.values(data.formations.formationsLongues.aws)
+let formationL2 = Object.values(data.formations.formationsLongues.codingSchool)
+let formationL3 = Object.values(data.formations.formationsLongues.marketingDigital)
+let formationL4 = Object.values(data.formations.formationsLongues.salesforceAdmin)
+
+// let newFL = formationL
 
 let formationC = Object.values(data.formations.formationsCourtes)
-let selectionC = formationC.slice(0, 6)
+let selectionC = formationC.slice(0, 5)
 let selectionCFinal = Array.from(selectionC)
 
+let ltableau = [formationL, formationL2, formationL3, formationL4]
 
 
 
@@ -16,21 +22,24 @@ let btnC = document.querySelectorAll("button")[2]
 
 
 btnL.addEventListener("click", () => {
+    textDeFormation.innerHTML = ""
 
-    formationL.forEach(element => {
+    ltableau.forEach(element => {
         let list = document.createElement("li")
         list.innerHTML = element
         textDeFormation.appendChild(list)
     });
-    textDeFormation = ""
 })
 
 btnC.addEventListener("click", () => {
+    textDeFormation.innerHTML = ""
     selectionCFinal.forEach(element => {
         let list = document.createElement("li")
-        console.log(element);
-        list.innerHTML = element
+        list.innerHTML = element.nom
+        let liste = document.createElement("li")
+        liste.innerHTML = element.pack
+        liste.style.color = "purple"
         textDeFormation.appendChild(list)
+        textDeFormation.appendChild(liste)
     });
-    textDeFormation = ""
 })
